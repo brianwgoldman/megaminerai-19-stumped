3ae33d41f5619822ab67046ac9f977517cf3e409
Brian W. Goldman <brianwgoldman@acm.org>
2017-04-10 21:26:17 -0600

    Formatting, commenting, and README.md.

--- README.md
+++ README.md
@@ -1,68 +1,13 @@
-# Stumped Python 3 Client
+# the-goldman-clause's Stumped Python 3 Client
 
-This is the root of you AI. Stay out of the `joueur/` folder, it does most of the heavy lifting to play on our game servers. Your AI, and the game objects it manipulates are all in `games/stumped/`, with your very own AI living in `games/stumped/ai.py` for you to make smarter.
+This was my first place submission to [MegaMinerAI](http://siggame.io/blog/) 19: Stumped,
+run by SIG-Game of Missouri University of Science and Technology. The only
+real logic I added was in `games/stumped/ai.py`, although I did update
+`games/stumped/tile.py` and `games/stumped/beaver.py` to allow for sorting
+and hashing. The only changes I have made since the end of competition
+was:
 
-## How to Run
-
-This client has been tested and confirmed to work on the Campus rc##xcs213 Linux machines, but it can work on your own Windows/Linux/Mac machines if you desire.
-
-### Linux
-
-```
-./testRun MyOwnGameSession
-```
-
-For Linux, a recent version of `python3` should work. It has been tested on 3.4.3 extensively, but should work with >= 3.2. The normal 'python' usually refers to Python 2.7.X, so make sure you have the python**3** installed.
-
-### Windows
-
-On Windows you'll need some version of Python 3. As with the Linux version, [3.4.3][343] has been tested against extensively, however there's no none reason why it would not work on newer versions. Install that and ensure that python is set up in your Environmental Variables as 'python3', then
-
-```
-python3 main.py Stumped -s r99acm.device.mst.edu -r MyOwnGameSession
-```
-
-## Make
-
-There is a `Makefile` provided, but it is empty as python is an interpreted language. If you want to add `make` steps feel free to, but you may want to check with an Arena dev to ensure the Arena has the packages you need to use in `make`.
-
-### Vagrant
-
-Install [Vagrant][vagrant] and [Virtualbox][virtualbox] in order to use the Vagrant configuration we provide which satisfies all build dependencies inside of a virtual machine. This will allow for development with your favorite IDE or editor on your host machine while being able to run the client inside the virtual machine. Vagrant will automatically sync the changes you make into the virtual machine that it creates. In order to use vagrant **after installing the aforementioned requirements** simply run from the root of this client:
-
-```bash
-vagrant up
-```
-
-and after the build has completed you can ssh into the virtual environment by running:
-
-```bash
-vagrant ssh
-```
-
-From there you will be in a Linux environment that has all the dependencies you'll need to build and run this client.
-
-When the competition is over, or the virtual environment becomes corrupted in some way, simply execute `vagrant destroy` to delete the virtual machine and its contents.
-
-For a more in depth guide on using vagrant, take a look at [their guide][vagrant-guide]
-
-#### Windows
-
-Using Vagrant with Windows can be a bit of a pain. Here are some tips:
-
-* Use an OpenSSH compatible ssh client. We recommend [Git Bash][gitbash] to serve double duty as your git client and ssh client
-* Launch the terminal of your choice (like Git Bash) as an Administrator to ensure the symbolic links can be created when spinning up your Vagrant virtual machine
-
-## Other Notes
-
-It is possible that on your Missouri S&T S-Drive this client will not run properly. This is not a fault with the client, but rather the school's S-Drive implementation changing some file permissions during run time. We cannot control this. Instead, we recommend cloning your repo outside the S-Drive and use an SCP program like [WinSCP][winscp] to edit the files in Windows using whatever IDE you want if you want to code in Windows, but compile in Linux.
-
-The only file you should ever modify to create your AI is the `ai.py` file. All the other files are needed for the game to work. In addition, you should never be creating your own instances of the Game's classes, nor should you ever try to modify their variables. Instead, treat the Game and its members as a read only structure that represents the game state on the game server. You interact with it by calling the game functions.
-
-[343]: https://www.python.org/downloads/release/python-343/
-[winscp]: https://winscp.net/eng/download.php
-[vagrant]: https://www.vagrantup.com/downloads.html
-[virtualbox]: https://www.virtualbox.org/wiki/Downloads
-[vagrant-guide]: https://www.vagrantup.com/docs/getting-started/up.html
-[virtualbox]: https://www.virtualbox.org/wiki/Downloads
-[gitbash]: https://git-scm.com/downloads
+1. White space formatting.
+2. Removal of a few unused functions.
+3. Removal of some print statements and commented out code.
+4. Added some comments to improve readability.
--- games/stumped/ai.py
+++ games/stumped/ai.py
@@ -38,7 +38,7 @@ def opposite(direction):
         return EAST
     else:
         raise Exception("Unknown direction: " + direction)
-    
+
 
 def tile_distance(t1, t2):
     return abs(t1.x - t2.x) + abs(t1.y - t2.y)
@@ -47,15 +47,16 @@ def can_act(beaver):
     return beaver and beaver.turns_distracted == 0 and beaver.health > 0 and not try_suicide(beaver)
 
 def try_suicide(beaver):
+    """If you are fully trapped and you're already on a lodge, just give up."""
     for neighbor in beaver.tile.get_neighbors():
         if not neighbor._spawner and not neighbor._lodge_owner:
             return False
     if not beaver.tile.lodge_owner:
         return False
-    print("SUICIDE!")
     return True
 
 def permablocked(tile):
+    """If all neighbors are immovable, impathable objects"""
     for neighbor in tile.get_neighbors():
         if not neighbor._spawner and not neighbor._lodge_owner:
             return False
@@ -93,7 +94,7 @@ class AI(BaseAI):
         Returns
             str: The name of your Player.
         """
-        return "the-goldman-clause"  # REPLACE THIS WITH YOUR TEAM NAME
+        return "the-goldman-clause"
 
     def start(self):
         """ This is called once the game starts and your AI knows its playerID and game. You can initialize your AI here.
@@ -114,7 +115,6 @@ class AI(BaseAI):
         """
         # replace with your end logic
 
-
     def my_lodge(self, tile):
         return tile.lodge_owner and tile.lodge_owner == self.player
 
@@ -122,9 +122,11 @@ class AI(BaseAI):
         return tile.lodge_owner and tile.lodge_owner == self.player.opponent
 
     def spawn(self):
+        """
+        Perform spawn action. Prioritizes lodge closest to a branch spawner.
+        Tries to keep an even number of Builders and Fighters, ties going to Builders.
+        """
         can_spawn = {lodge for lodge in self.player.lodges if not lodge.beaver and not permablocked(lodge)}
-
-
         enemies = [beaver.tile for beaver in self.player.opponent.beavers]
         while self.alive_beavers < self.game.free_beavers_count:
             path = []
@@ -152,43 +154,32 @@ class AI(BaseAI):
         if self.enough_to_build(beaver, beaver.tile) and not beaver.tile.lodge_owner:
             beaver.build_lodge()
 
-    def try_pick_up(self, beaver):
-        if not can_act(beaver) or beaver.actions == 0 or load(beaver) >= beaver.job.carry_limit:
-            return
-        neighbors = beaver.tile.get_neighbors()
-        neighbors.append(beaver.tile)
-        branch_tiles = [tile for tile in neighbors if tile.branches > 0 and not self.my_lodge(tile)]
-        if branch_tiles:
-            tile = random.choice(branch_tiles)
-            # print('{} picking up branches'.format(beaver))
-            beaver.pickup(tile, 'branches', 1)
-
     def try_pickup_opponent(self, beaver):
+        """Attempt to take branches from opponent lodges, prioritizing the lowest health lodge."""
         if not can_act(beaver) or beaver.actions == 0 or load(beaver) >= beaver.job.carry_limit:
             return
         neighbors = beaver.tile.get_neighbors()
         branch_tiles = [tile for tile in neighbors if tile.branches > 0 and self.their_lodge(tile)]
         if branch_tiles:
             tile = min(branch_tiles, key=lambda tile: tile.branches)
-            print('{} picking up branches from opponent'.format(beaver))
             beaver.pickup(tile, 'branches', min(tile.branches, beaver.job.carry_limit - load(beaver)))
-#         # try to pickup food
-#         elif tile.food > 0:
-#             print('{} picking up food'.format(beaver))
-#             beaver.pickup(tile, 'food', 1)
-#             break
 
     def try_harvest(self, beaver, type):
-        # if we can carry more, try to harvest something
+        """Attempt harvest from a branch spawner, prioritizing the highest health spawner."""
         if not can_act(beaver) or beaver.actions == 0 or load(beaver) >= beaver.job.carry_limit:
             return
-        harvest_tiles = [tile for tile in beaver.tile.get_neighbors() if tile.spawner and tile.spawner.health > 1 and tile.spawner.type == type]
+        harvest_tiles = [tile for tile in beaver.tile.get_neighbors()
+                         if tile.spawner and tile.spawner.health > 1 and tile.spawner.type == type]
         if harvest_tiles:
             tile = max(harvest_tiles, key=lambda tile: tile.spawner.health)
-            # print('{} harvesting {}'.format(beaver, tile.spawner))
             beaver.harvest(tile.spawner)
 
     def try_attack(self, beaver):
+        """
+        Always try to steal branches from the opponents lodge first,
+        but then try to attack the lowest health opponent in range.
+        Ignore distracted beavers.
+        """
         self.try_pickup_opponent(beaver)
         if not can_act(beaver) or beaver.actions == 0:
             return
@@ -198,21 +189,28 @@ class AI(BaseAI):
                         tile.beaver.recruited and tile.beaver.health > 0 and beaver.turns_distracted == 0]
         if target_tiles:
             target_tile = min(target_tiles, key=lambda tile: tile.beaver.health)
-            print('{} attacking {}'.format(beaver, target_tile.beaver))
             beaver.attack(target_tile.beaver)
 
     def attack_move(self, beaver, path, last_step):
+        """
+        Makes sure you try attack at every step along the path.
+        If last_step is False, don't try to move to the final tile in the path.
+        Assumes the first step in the path is the beaver's current location.
+        """
         self.try_attack(beaver)
         for step in path[1:]:
             if move_cost(beaver.tile, step) > beaver.moves:
                 break
             if step is path[-1] and not last_step:
                 break
-            # print('Moving {} towards {}'.format(beaver, path[-1]))
             beaver.move(step)
             self.try_attack(beaver)
 
     def try_move_off_lodge(self, beaver):
+        """
+        If you're on a lodge, and one of your neighbors isn't a lodge, move off.
+        Prioritize closer to branch spawners.
+        """
         if not can_act(beaver) or not beaver.tile.lodge_owner:
             return
         non_lodge_tiles = [tile for tile in beaver.tile.get_neighbors() if pathable(tile)]
@@ -224,34 +222,27 @@ class AI(BaseAI):
                     beaver.move(step)
 
     def branch_spawners(self):
-        return {tile for tile in self.game.tiles if tile.spawner and tile.spawner.health > 1 and tile.spawner.type == BRANCHES}
+        return {tile for tile in self.game.tiles
+                if tile.spawner and tile.spawner.health > 1 and tile.spawner.type == BRANCHES}
 
     def gather_branches(self, beaver):
-        # print("Gather mode")
+        """Full turn command that tries to move to the nearest branch spawner and get branches."""
         self.try_attack(beaver)
         self.try_harvest(beaver, BRANCHES)
-        goals = [tile for tile in self.game.tiles if tile.spawner and tile.spawner.health > 1 and tile.spawner.type == BRANCHES]
+        goals = [tile for tile in self.game.tiles
+                 if tile.spawner and tile.spawner.health > 1 and tile.spawner.type == BRANCHES]
         path = self.find_path([beaver.tile], goals)
         self.attack_move(beaver, path, last_step=False)
         self.try_harvest(beaver, BRANCHES)
-
-    def steal_branches(self, beaver):
-        # print("Steal Mode!")
-        self.try_attack(beaver)
-        self.try_pickup_opponent(beaver)
-        goals = [tile for tile in self.game.tiles if self.their_lodge(tile)]
-        if not goals:
-            return
-        path = self.find_path([beaver.tile], goals)
-        if not path:
-            return
-        self.attack_move(beaver, path, last_step=False)
-        self.try_pickup_opponent(beaver)
 
     def pile_branches(self, beaver):
-        # print("Pile mode")
+        """
+        Full turn command that tries to drop off branches intelligently.
+        If there is a near by pile, move and add to it, otherwise just dump next to you.
+        """
         self.try_attack(beaver)
-        goals = [tile for tile in self.closer_to_me if droppable(tile) and not self.my_lodge(tile) and not self.their_lodge(tile)]
+        goals = [tile for tile in self.closer_to_me
+                 if droppable(tile) and not self.my_lodge(tile) and not self.their_lodge(tile)]
         path = self.find_path([beaver.tile], goals)
         better = [tile for tile in goals if tile.branches > 0]
         if better:
@@ -261,8 +252,6 @@ class AI(BaseAI):
                 more_needed = self.player.branches_to_build_lodge - beaver.branches - path[-1].branches
                 if more_needed / beaver.job.carry_limit > turns_away:
                     path = better_path
-                else:
-                    print("\nDrop it now!\n")
         if not path:
             return
         if self.enough_to_build(beaver, path[-1]):
@@ -272,27 +261,16 @@ class AI(BaseAI):
             self.attack_move(beaver, path, last_step=False)
             if len(path) > 1 and tile_distance(beaver.tile, path[-1]) < 2:
                 if beaver.actions > 0 and beaver.branches > 0:
-                    print("Dropping off")
                     beaver.drop(path[-1], 'branches', beaver.branches)
 
-    def enemy_tiles(self, job):
-        return [beaver.tile for beaver in self.player.opponent.beavers if beaver.job is job and beaver.health > 0 and beaver.turns_distracted == 0]
-
     def go_hunting(self, beaver):
+        """Full turn command to seek out and attack enemy lodges."""
         self.try_attack(beaver)
         path = self.find_path([beaver.tile], [tile for tile in self.game.tiles if self.their_lodge(tile)])
         if not path:
             path = self.find_path([beaver.tile], [enemy.tile for enemy in self.player.opponent.beavers
                                                   if enemy.health > 0 and enemy.turns_distracted == 0])
         self.attack_move(beaver, path, last_step=False)
-#         ordering = [self.HUNGRY, self.HOT_LADY, self.FIGHTER, self.BULKY, self.BUILDER, self.SWIFT, self.BASIC]
-#         for job in ordering:
-#             goal_tiles = self.enemy_tiles(job)
-#             if goal_tiles:
-#                 path = self.find_path([beaver.tile], goal_tiles)
-#                 if path:
-#                     self.attack_move(beaver, path, last_step=False)
-#                     return
 
     def setup(self):
         for job in self.game.jobs:
@@ -323,19 +301,19 @@ class AI(BaseAI):
         Returns:
             bool: Represents if you want to end your turn. True means end your turn, False means to keep your turn going and re-call this function.
         """
-
-        # First let's do a simple print statement telling us what turn we are on
         print('My Turn {}'.format(self.game.current_turn))
         self.setup()
         self.set_nearest_beaver()
         self.spawn()
-        for beaver in self.player.beavers:  # if we have a beaver, and it's not distracted, and it is alive (health greater than 0)
+        for beaver in self.player.beavers:
             if not can_act(beaver):
                 continue
+            # Always try to build a lodge if you can
             self.try_build_lodge(beaver)
             if load(beaver) >= beaver.job.carry_limit:
                 if beaver.job in self.COMBAT:
-                    print("Chuck it")
+                    # Combat beavers don't have time for smart piling,
+                    # they just dump stuff.
                     self.try_build_lodge(beaver)
                     beaver.drop(beaver.tile, BRANCHES, beaver.branches)
                 else:
@@ -346,12 +324,21 @@ class AI(BaseAI):
             else:
                 self.gather_branches(beaver)
         for beaver in self.player.beavers:
+            # If you still have moves left, try not to end your turn
+            # on a lodge
             self.try_move_off_lodge(beaver)
+        # Calls spawn a second time just in case some lodges have freed up.
         self.spawn()
         print('Done with our turn')
-        return True # to signify that we are truly done with this turn
+        return True  # to signify that we are truly done with this turn
 
     def find_path(self, start_tiles, goal_tiles):
+        """
+        Given a list of starting locations and a list of ending locations,
+        find the shortest path between them. Uses a priority queue to
+        do Uniform Cost Search (think Breadth First Search, but with movement
+        cost included).
+        """
         open_q = [(0, tile) for tile in start_tiles]
         heapq.heapify(open_q)
         goals = {tile for tile in goal_tiles}
@@ -378,6 +365,11 @@ class AI(BaseAI):
         return []
 
     def set_nearest_beaver(self):
+        """
+        Builds the 'closer_to_me' set of tiles, which are all tiles that are
+        distance-wise closer to my units than theirs. Pretty sure this
+        wasn't useful at all.
+        """
         open_q = [(0, beaver.tile, beaver) for beaver in self.game.beavers]
         heapq.heapify(open_q)
         source = defaultdict(lambda: (None, 100000000))
